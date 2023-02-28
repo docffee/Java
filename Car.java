8a130998431c9038d6870618f739e552d79b5345
Alvin Long <52389038+docffee@users.noreply.github.com>
2023-02-27 20:01:46 -0500

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,25 @@
+
+public class Car {
+	public Car() {
+		int yearModel = 2000;
+		String make = "Toyota";
+		int speed = 20;
+	}
+	public void getSpeed() {
+		
+	}
+	
+	public void accelerate() {
+		
+	}
+	
+	public void brake() {
+		
+	}
+	public static void main(String[] args) {
+		Car car = new Car();
+		
+	}
+	
+
+}
